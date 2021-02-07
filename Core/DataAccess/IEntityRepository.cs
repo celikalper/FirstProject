f894bd8818dfd87361b3827cd48828bf1dc3126d
Alper Çelik <alper.celikk1@outlook.com>
2021-02-07 19:01:09 +0300

    6 Şubat 9.Ders

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
-using Entities.Abstract;
+using Core.Entities;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //Generic constraint
     //class : referans tip demek
